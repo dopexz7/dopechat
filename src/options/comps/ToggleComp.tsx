eec7548d5236fe4a162253b57be0b79208c94d0b
DopeXZ <93769793+dopexz7@users.noreply.github.com>
2022-07-22 17:56:21 +0300

    Update ToggleComp.tsx

--- src/options/comps/ToggleComp.tsx
+++ src/options/comps/ToggleComp.tsx
@@ -12,16 +12,24 @@ const ToggleComp: FC<any> = ({
     return (
         <div className="border-[1px] border-white border-opacity-5 p-3 rounded-3xl text-white text-base font-medium flex flex-row items-center tracking-wider mb-3 mt-3">
             <div className="px-3">{title}</div>
-            <div
-                onClick={() => onChange(!checked)}
-                className={`${
-                    checked
-                        ? "bg-white text-darker-purple "
-                        : "hover:bg-white hover:text-darker-purple text-white"
-                } backdrop-blur-sm border-[1px] border-white border-opacity-5 w-max shadow-2xl px-4 py-1.5 rounded-2xl font-medium  text-base cursor-pointer duration-300 ml-auto`}
-            >
-                {checked ? "Disable" : "Enable"}
-            </div>
+            {process.env.BROWSER === "firefox" && title === "Emote menu" ? (
+                <div
+                    className={` text-white backdrop-blur-sm border-[1px] border-white border-opacity-5 w-max shadow-2xl px-4 py-1.5 rounded-2xl font-medium  text-base duration-300 ml-auto`}
+                >
+                    Emote menu is disabled for Firefox
+                </div>
+            ) : (
+                <div
+                    onClick={() => onChange(!checked)}
+                    className={`${
+                        checked
+                            ? "bg-white text-darker-purple "
+                            : "hover:bg-white hover:text-darker-purple text-white"
+                    } backdrop-blur-sm border-[1px] border-white border-opacity-5 w-max shadow-2xl px-4 py-1.5 rounded-2xl font-medium  text-base cursor-pointer duration-300 ml-auto`}
+                >
+                    {checked ? "Disable" : "Enable"}
+                </div>
+            )}
         </div>
     );
 };
