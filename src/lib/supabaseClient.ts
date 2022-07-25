8ede49536be1b3bcb0ec98f30d0651129880d204
DopeXZ <93769793+dopexz7@users.noreply.github.com>
2022-07-25 20:24:03 +0300

    -

--- src/lib/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl: string = "https://emxllayyisdskjtscvck.supabase.co";
-const supabaseAnonKey: string =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVteGxsYXl5aXNkc2tqdHNjdmNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTYyNjI2NzIsImV4cCI6MTk3MTgzODY3Mn0.BI4xM-cQS5zrhIIYev_dWaPD65eHTadfvl1zlSEYFi4";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
