e32998b7b69eacbefa874b1d915cdf4874ee55e0
DopeXZ <93769793+dopexz7@users.noreply.github.com>
2022-07-25 20:25:30 +0300

    Create supabaseClient.ts

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,5 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl: any = process.env.SUPABASE_URL;
+const supabaseAnonKey: any = process.env.SUPABASE_ANON_KEY;
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
