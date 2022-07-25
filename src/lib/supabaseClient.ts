8940eb9bad61eb57b16690f6829eaa5850dbc78b
DopeXZ <93769793+dopexz7@users.noreply.github.com>
2022-07-26 00:59:11 +0300

    Revert "Revert "1.2.3""
    This reverts commit 62ca2252d86e90519fa2bd3b7104e0cab5b7f834.

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl: any = process.env.SUPABASE_URL;
-const supabaseAnonKey: any = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl: string = "https://emxllayyisdskjtscvck.supabase.co";
+const supabaseAnonKey: string =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVteGxsYXl5aXNkc2tqdHNjdmNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTYyNjI2NzIsImV4cCI6MTk3MTgzODY3Mn0.BI4xM-cQS5zrhIIYev_dWaPD65eHTadfvl1zlSEYFi4";
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
